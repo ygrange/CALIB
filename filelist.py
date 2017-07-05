4151813a4a32b4851cd40b17fbd3b86c9a6a652a
Yan Grange <grange@astron.nl>
2017-07-05 16:57:07 +0200

    Make filelist.py more configurable

--- filelist.py
+++ filelist.py
@@ -1,7 +1,13 @@
+import ConfigParser
+
+
 class FileReturner(object):
 
-    def __init__(self):
-        self.files = {"calibrator": "L12345C.MS", "target": "L12345T.MS"}
+    def __init__(self, input_files='/tmp/inputfiles.dat'):
+        parser = ConfigParser.ConfigParser()
+        parser.read(input_files)
+        self.files = {"calibrator": parser.get('files', 'calibrators'),
+                      "target": parser.get('files', 'targets')}
 
     def get(self, filetype):
         return self.files[filetype]
