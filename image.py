7d20f4133b4e1c1f736467c84e2bd24d96ece805
Yan Grange <grange@astron.nl>
2017-08-02 16:29:19 +0200

    Replaced fileroot by output dir for images

--- image.py
+++ image.py
@@ -27,15 +27,16 @@ parser = argparse.ArgumentParser(description="Calibrate gains.")
 parser.add_argument("-m", "--ms", required=True, help="Input measurement set")
 parser.add_argument("-b", "--beamsize", type=lambda s: [int(item) for item in s.split(',')],
                     required=True, help="List of beam sizes in arcsec")
-parser.add_argument("-f", "--fov", required=True, help="Field of view")
-parser.add_argument("-r", "--fileroot", required=True, help="Output file root")
+parser.add_argument("-f", "--fov", required=True, help="Field of view", type=float)
+parser.add_argument("-o", "--outdir", required=True, help="Output directory")
 parser.add_argument("-x", "--donemark_input", required=False, help="Input donemark dependency. (not required)")
 args = parser.parse_args()
 
 if args.donemark_input:
     check_donemark(args.donemark_input)
 
-CALIB.wsclean_image(ms=args.ms,
-                    beamsize=args.beamsize,
-                    field_of_view=args.fov,
-                    fileroot=args.fileroot)
+for beamsize in args.beamsize:
+    CALIB.wsclean_image(ms=args.ms,
+                        beamsize=beamsize,
+                        field_of_view=args.fov,
+                        outdir=args.outdir)
